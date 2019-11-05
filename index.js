f0115c7d60728cc126e4c68446dc79893708f4a5
Anlimou <halimoudu73@hotmail.fr>
2019-11-05 15:47:01 +0100

    City
    Print city

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-var express = require('express')
-var app = express()
+var express = require("express");/* npm install express */
+var csv = require('csv-express')/* npm install csv-express*/
+var fetchUrl = require("fetch").fetchUrl
 
-var url = 'https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_72h_poll_princ/FeatureServer/0/query?'
+const fs = require('fs')
 
-app.get(url, function (req, res) {
+var app = express();
+/*
+app.get('/', function (req, res) {
     res.send('Hello World!')
   })
 
+app.get('/user/:name', function(req, res) {
+	var age=''+req.query.age;
+	if(age!=="undefined" && age.trim().length){
+	res.send('Hello '  + req.params.name + ' tu as ' + age +' ans');
+	}else{
+	(res.send('Hello '  + req.params.name));
+	}
+})*/
+
+app.get('/index', function(req,res) {
+	fs.readFile('index.html', function(err, html) {
+	if(err){throw err;}
+	res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(html)
+            res.end()
+	})
+
+})
+
+app.get('/city' ,function(req,res){
+    var url="https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&limit=15";
+    fetchUrl(url , function(error, meta, body){
+		json = JSON.parse(body);
+        res.write(json.features[0].properties.city)
+        res.end()
+    })
+})
+
+app.get('/names', function(req,res) {
+	res.format({
+        'application/json': function () {
+            res.json([{name : 'toto'}, {name : 'baptiste'}, {name : 'gabriel'}]);
+        },
+
+        'application/csv': function () {
+            res.csv([{name : 'toto'}, {name : 'baptiste'}, {name : 'gabriel'}]);
+        }
+    })
+})
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
-  })
+  });
